055953485ecd47085bd78ee20096027b3d015ffd
Chris O'Donnell <chris.bowdoin@gmail.com>
2015-02-14 11:08:03 -0500

    Added a whole bunch of actions.

--- database.go
+++ database.go
@@ -1,72 +1,73 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	_ "github.com/lib/pq"
-	"log"
-)
-
-type CornerDB struct {
-	db *sql.DB
-}
-
-func Connect() *sql.DB {
-	db, err := sql.Open("postgres",
-		"dbname=chris user=chris host=/var/run/postgresql sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-	defer db.Close()
-	return db
-}
-
-func (db *CornerDB) AddUser(nick string) {
-	rows, err := db.Query("select nick from users where nick = $1", nick)
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-	defer rows.Close()
-	var dbNick string
-	rows.Next()
-	err := rows.Scan(&dbNick)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(dbNick)
-}
-
-func main() {
-	db, err := sql.Open("postgres",
-		"dbname=chris user=chris host=/var/run/postgresql sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Connection error: %s\n", err)
-		return
-	}
-
-	rows, err := db.Query("SELECT * FROM users")
-	defer rows.Close()
-	for rows.Next() {
-		var nick string
-		var points int
-		err = rows.Scan(&nick, &points)
-		if err != nil {
-			fmt.Printf("Connection error: %s\n", err)
-			return
-		}
-		fmt.Printf("Nick: %s\tPoints: %d\n", nick, points)
-	}
-	err = rows.Err()
-	if err != nil {
-		fmt.Printf("Connection error: %s\n", err)
-		return
-	}
-
-}
+//
+// import (
+// 	"database/sql"
+// 	"fmt"
+// 	_ "github.com/lib/pq"
+// 	"log"
+// )
+//
+// type CornerDB struct {
+// 	db *sql.DB
+// }
+//
+// func Connect() *sql.DB {
+// 	db, err := sql.Open("postgres",
+// 		"dbname=chris user=chris host=/var/run/postgresql sslmode=disable")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 		return nil
+// 	}
+// 	defer db.Close()
+// 	return db
+// }
+//
+// func (db *CornerDB) AddUser(nick string) {
+// 	rows, err := db.Query("select nick from users where nick = $1", nick)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 		return nil
+// 	}
+// 	defer rows.Close()
+// 	var dbNick string
+// 	rows.Next()
+// 	err := rows.Scan(&dbNick)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	log.Println(dbNick)
+// }
+//
+// func main() {
+// 	db, err := sql.Open("postgres",
+// 		"dbname=chris user=chris host=/var/run/postgresql sslmode=disable")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
+// 	err = db.Ping()
+// 	if err != nil {
+// 		fmt.Println("Connection error: %s\n", err)
+// 		return
+// 	}
+//
+// 	rows, err := db.Query("SELECT * FROM users")
+// 	defer rows.Close()
+// 	for rows.Next() {
+// 		var nick string
+// 		var points int
+// 		err = rows.Scan(&nick, &points)
+// 		if err != nil {
+// 			fmt.Printf("Connection error: %s\n", err)
+// 			return
+// 		}
+// 		fmt.Printf("Nick: %s\tPoints: %d\n", nick, points)
+// 	}
+// 	err = rows.Err()
+// 	if err != nil {
+// 		fmt.Printf("Connection error: %s\n", err)
+// 		return
+// 	}
+//
+// }
--- handlers.go
+++ handlers.go
@@ -1,19 +1,67 @@
 package main
 
 import (
-	irc "github.com/fluffle/goirc/client"
+	"bytes"
+	"fmt"
 	"regexp"
+	"strconv"
+
+	irc "github.com/fluffle/goirc/client"
 )
 
 var botHandlers = []irc.HandlerFunc{
 	Heart,
 	RandomPage,
 	Hug,
+	SelfEsteem,
+	Glitter,
+	CreateAction("tickle", "ties %s up to the bedpost tightly and takes out a feather. Time for some tickles!"),
+	CreateAction("lick", "jumps on top of %s and gives them a big slobbery lick!"),
+	CreateAction("peck", "sneaks up on %s and delicately pecks them on the cheek"),
+	CreateAction("slap", "bops %s on the head and reminds them that violence is bad!"),
+	CreateAction("paint", "pulls out the paint set as %s lays naked on the couch posing"),
+	CreateAction("landlust", "lusts %s!"),
+	CreateAction("beer", "goes to the fridge, grabs a fresh bottle, pops the cap off and hands it to %s. Enjoy!"),
+	CreateAction("shuggle", "shuggles %s!"),
+	CreateAction("pinch", "sneaks behind %s and gives a little pinch on the butt!"),
+	CreateAction("snuggle", "curls up next to %s and snuggles closely"),
+	CreateAction("pillowfight", "waits until they are sleeping and SMOTHERS %s WITH A PILLOW!"),
 }
 
 func addBotHandlers(conn *irc.Conn) {
 	for _, h := range botHandlers {
 		conn.HandleFunc("privmsg", h)
+	}
+}
+
+func Colorize(text string) string {
+	var buffer bytes.Buffer
+
+	for i, char := range text {
+		buffer.WriteString("\x03")
+		buffer.WriteString(strconv.Itoa((i % 14) + 2))
+		buffer.WriteRune(char)
+	}
+	buffer.WriteString("\x03")
+	return buffer.String()
+}
+
+func CreateAction(name string, message string) irc.HandlerFunc {
+	return func(conn *irc.Conn, line *irc.Line) {
+		isAction := regexp.MustCompile("!" + name + " (\\S+)")
+		matches := isAction.FindStringSubmatch(line.Text())
+		if len(matches) < 2 {
+			return
+		} else {
+			conn.Action(line.Target(), fmt.Sprintf(message, matches[1]))
+		}
+	}
+}
+
+func Glitter(conn *irc.Conn, line *irc.Line) {
+	containsGlitter := regexp.MustCompile(`!glitter`)
+	if containsGlitter.MatchString(line.Text()) {
+		conn.Action(line.Target(), "throws some "+Colorize("¸¸.•*¨*•¸¸.•*¨*• rainbow glitter ¸¸.•*¨*•¸¸.•*¨*•")+" into the air!")
 	}
 }
 
@@ -25,7 +73,7 @@ func Heart(conn *irc.Conn, line *irc.Line) {
 }
 
 func Hug(conn *irc.Conn, line *irc.Line) {
-	isHug := regexp.MustCompile(`!hug (.+)`)
+	isHug := regexp.MustCompile(`!hug (\S+)`)
 	matches := isHug.FindStringSubmatch(line.Text())
 	if len(matches) < 2 {
 		return
@@ -34,6 +82,14 @@ func Hug(conn *irc.Conn, line *irc.Line) {
 		conn.Action(line.Target(), "hugs everyone!")
 	} else {
 		conn.Action(line.Target(), "hugs "+matches[1])
+	}
+}
+
+func SelfEsteem(conn *irc.Conn, line *irc.Line) {
+	isEsteem := regexp.MustCompile(`!selfesteem`)
+	matches := isEsteem.FindStringSubmatch(line.Text())
+	if len(matches) > 0 {
+		conn.Privmsg(line.Target(), "GOOOOOO "+line.Nick+"! YOU GOT DIS!")
 	}
 }
 
