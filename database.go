884a2048aed416d081ce9d374ffddf8d0a7a1ec9
Chris O'Donnell <chris.bowdoin@gmail.com>
2015-04-29 16:16:14 -0400

    Add basic sqlite3 database functionality for points.

--- database.go
+++ database.go
@@ -1,73 +1,95 @@
 package main
 
-//
-// import (
-// 	"database/sql"
-// 	"fmt"
-// 	_ "github.com/lib/pq"
-// 	"log"
-// )
-//
-// type CornerDB struct {
-// 	db *sql.DB
-// }
-//
-// func Connect() *sql.DB {
-// 	db, err := sql.Open("postgres",
-// 		"dbname=chris user=chris host=/var/run/postgresql sslmode=disable")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		return nil
-// 	}
-// 	defer db.Close()
-// 	return db
-// }
-//
-// func (db *CornerDB) AddUser(nick string) {
-// 	rows, err := db.Query("select nick from users where nick = $1", nick)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		return nil
-// 	}
-// 	defer rows.Close()
-// 	var dbNick string
-// 	rows.Next()
-// 	err := rows.Scan(&dbNick)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	log.Println(dbNick)
-// }
-//
-// func main() {
-// 	db, err := sql.Open("postgres",
-// 		"dbname=chris user=chris host=/var/run/postgresql sslmode=disable")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer db.Close()
-// 	err = db.Ping()
-// 	if err != nil {
-// 		fmt.Println("Connection error: %s\n", err)
-// 		return
-// 	}
-//
-// 	rows, err := db.Query("SELECT * FROM users")
-// 	defer rows.Close()
-// 	for rows.Next() {
-// 		var nick string
-// 		var points int
-// 		err = rows.Scan(&nick, &points)
-// 		if err != nil {
-// 			fmt.Printf("Connection error: %s\n", err)
-// 			return
-// 		}
-// 		fmt.Printf("Nick: %s\tPoints: %d\n", nick, points)
-// 	}
-// 	err = rows.Err()
-// 	if err != nil {
-// 		fmt.Printf("Connection error: %s\n", err)
-// 		return
-// 	}
-//
-// }
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+type CornerDB struct {
+	db *sql.DB
+}
+
+type User struct {
+	nick   string
+	points int
+}
+
+func Connect() *sql.DB {
+	db, err := sql.Open("sqlite3", "./users.db")
+	checkErr(err)
+	return db
+}
+
+func (db *CornerDB) GetUser(nick string) *User {
+	rows, err := db.db.Query("select nick,points from users where nick = $1", nick)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	defer rows.Close()
+	var dbNick string
+	var dbPoints int
+	if rows.Next() {
+		err = rows.Scan(&dbNick, &dbPoints)
+		checkErr(err)
+	} else {
+		return nil
+	}
+	return &User{dbNick, dbPoints}
+}
+
+func (db *CornerDB) GetPoints(nick string) int {
+	user := db.GetUser(nick)
+	if user == nil {
+		return 0
+	}
+	return user.points
+}
+
+func (db *CornerDB) CreateUser(nick string) {
+	if db.GetUser(nick) != nil {
+		return
+	}
+	_, err := db.db.Exec("insert into users(nick, points) values(?, ?)", nick, 0)
+	checkErr(err)
+}
+
+func (db *CornerDB) SetPoints(nick string, points int) {
+	user := db.GetUser(nick)
+	if user == nil {
+		return
+	}
+	_, err := db.db.Exec("update users set points=? where nick=?", points, nick)
+	checkErr(err)
+}
+
+func (db *CornerDB) AddPoints(nick string, points int) {
+	user := db.GetUser(nick)
+	if user == nil {
+		return
+	}
+	db.SetPoints(nick, user.points+points)
+}
+
+func main() {
+	db := CornerDB{Connect()}
+	defer db.db.Close()
+
+	db.CreateUser("testuser")
+	db.SetPoints("testuser", 100)
+	db.AddPoints("testuser", -5)
+	user := db.GetUser("testuser")
+	if user == nil {
+		fmt.Println("User does not exist")
+	} else {
+		fmt.Printf("Got user: %s, %d\n", user.nick, user.points)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
